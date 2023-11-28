dfd0c6a2454036896c077ea14240887e66090f41
canizalesJd <canizalesjd@gmail.com>
2023-11-28 12:54:22 -0600

    Adding grid size validations and converting actions to functions

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ const grid = document.querySelector(".grid");
 const square = document.createElement("div");
 square.classList.add("square");
 
-// To do: convert this to user input
-let gridSize = 64;
-
 const createGrid = (gridSize) => {
   for (let i = 0; i < gridSize * gridSize; i++) {
     square.style.height = `${100 / gridSize}%`;
@@ -14,18 +11,45 @@ const createGrid = (gridSize) => {
   }
 };
 
-const sizeInput = document.querySelector(".grid-size");
-sizeInput.addEventListener("change", () => {
-  gridSize = sizeInput.value;
+const setSquaresBackground = (color) => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = color;
+    });
+  });
+};
+
+const adjustGridSize = (gridSize, color = "#707070") => {
   grid.innerHTML = "";
   createGrid(gridSize);
+  setSquaresBackground(color);
+};
+
+const sizeInput = document.querySelector(".grid-size");
+sizeInput.addEventListener("change", () => {
+  if (sizeInput.value > 100) {
+    sizeInput.value = 100;
+    alert("Grid size cannot exceed 100");
+    adjustGridSize(sizeInput.value);
+    return;
+  }
+  if (sizeInput.value < 1) {
+    sizeInput.value = 1;
+    alert("Grid size cannot be less than 1");
+    return;
+  }
+  if (isNaN(sizeInput.value)) {
+    alert("Please enter a number");
+    return;
+  }
+  adjustGridSize(sizeInput.value);
 });
 
-createGrid(gridSize);
+const init = () => {
+  let gridSize = 32;
+  createGrid(gridSize);
+  setSquaresBackground("#707070");
+};
 
-const squares = document.querySelectorAll(".square");
-squares.forEach((square) => {
-  square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "#707070";
-  });
-});
+init();
